5db183d8d2b332b5ca0657c804dddab9e9f60b1e
Jeffdev <robotzurg@gmail.com>
2021-01-28 12:23:32 -0700

    Fixed min-max

--- pinapl-bot/index.js
+++ pinapl-bot/index.js
@@ -1,10 +1,16 @@
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const client = new Discord.Client();
 let crateID;
 let crateUsrID;
-let intervalTime = (Math.floor(Math.random() * 1.008e+8) + 4.32e+7);
+let intervalTime = getRandomInt(4.32e+7, 1.008e+8)
 console.log(intervalTime);
 
 client.once('ready', () => {
@@ -16,8 +22,9 @@ const myFunction = function() {
 	(channel.send('📦 PINAPL CRATE 📦\n*React first to claim!*')).then((msg) => {
 		crateID = msg.id;
 	});
-	intervalTime = (Math.floor(Math.random() * 1.008e+8) + 4.32e+7);
+	intervalTime = getRandomInt(4.32e+7, 1.008e+8);
 	setTimeout(myFunction, intervalTime);
+	console.log(intervalTime);
 }
 setTimeout(myFunction, intervalTime);
 
@@ -36,7 +43,7 @@ client.on('message', async message => {
 				const reaction = collected.first();
 		
 				if (reaction.emoji.name === '🔑') {
-					let crateAmt = Math.floor(Math.random() * 30) + 1
+					let crateAmt = getRandomInt(1, 30)
 					otherCh.send(`<@145267507844874241>, <@${crateUsrID}> found ${crateAmt} <:pp:772971222119612416>!`)
 					message.channel.send(`<@${crateUsrID}> has claimed the crate.\nYou find **${crateAmt}** <:pp:772971222119612416>! Congratulations!`);
 				}
